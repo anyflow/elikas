d1faa9ed7a5fa8031010b18acedbb9926f0a1513
Park Hyunjeong <anyflow@gmail.com>
2024-07-27 12:06:19 +0900

    feat: add initial CR and CRD

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 
 # Elikas
 
-OpenAPI to Istio
+OpenAPI to Istio WasmPlugin, VirtualService converter
+
+## Test commands
+
+```bash
+# for dockebi
+PYTHONPATH=elikas OPENAPI_PATH=./tests/openapi.dockebi.yaml WASMPLUGIN_PATH=./tests/output.dockebi.yaml python -m elikas
+# for public-apigw
+PYTHONPATH=elikas OPENAPI_PATH=./tests/openapi.public-apigw.yaml WASMPLUGIN_PATH=./tests/output.public-apigw.yaml python -m elikas
+```
--- elikas/bizlogic.py
+++ elikas/bizlogic.py
@@ -6,51 +6,13 @@ from utilities import dump_yaml, load_yaml, wrap_values_and_conditions
 
 def create_wasmplugin() -> None:
     openapi = __validate(load_yaml(OPENAPI_PATH))
-
     app = openapi["x-tcn"]["app"]
     namespace = openapi["x-tcn"]["krakendEndpoint"]["backend"]["host"].split(".")[1]
     paths = openapi.get("paths", {}).keys()
 
-    def request_path_item(path):
-        if "{" in path:
-            pattern = re.sub(r"\{([^}]+)\}", r"[A-Za-z0-9_.-]*", path)
-            condition = f"request.url_path.matches('{pattern}')"
-        else:
-            condition = f"request.url_path == '{path}'"
-
-        return {
-            "value": path,
-            "condition": condition,
-        }
-
-    def request_apigroup_item(path):
-        if match := re.match(r"^\/(v[0-9]+)\/([^\/]+)(?:\/([^\/]+))*$", path):
-            pattern = "/v[0-9]+/" + (
-                f"{match.groups()[1]}/.*"
-                if match.groups()[2]
-                else f"{match.groups()[1]}"
-            )
-            return {
-                "value": match.groups()[1],
-                "condition": f"request.url_path.matches('{pattern}')",
-            }
-        else:
-            apigroup = path.split("/")[1]
-            pattern = f"/{apigroup}/.*"
-            return {
-                "value": apigroup,
-                "condition": f"request.url_path.matches('{pattern}')",
-            }
+    template = load_yaml(WASMPLUGIN_TEMPLATE_PATH)
 
     seen = set()
-    request_apigroups = []
-
-    for path in paths:
-        if str(apigroup := request_apigroup_item(path)) not in seen:
-            request_apigroups.append(apigroup)
-            seen.add(str(apigroup))
-
-    template = load_yaml(WASMPLUGIN_TEMPLATE_PATH)
     wasmplugin = {
         **template,
         "metadata": {
@@ -64,11 +26,16 @@ def create_wasmplugin() -> None:
                 "attributes": [
                     {
                         "output_attribute": "request_apigroup",
-                        "match": request_apigroups,
+                        "match": [
+                            item
+                            for path in paths
+                            if str(item := __request_apigroup_item(path)) not in seen
+                            and not seen.add(str(item))
+                        ],
                     },
                     {
                         "output_attribute": "request_path",
-                        "match": [request_path_item(path) for path in paths],
+                        "match": [__request_path_item(path) for path in paths],
                     },
                 ],
             },
@@ -83,3 +50,33 @@ def create_wasmplugin() -> None:
 def __validate(openapi: dict) -> dict:
     # TODO: Validate OpenAPI
     return openapi
+
+
+def __request_path_item(path):
+    if "{" in path:
+        pattern = re.sub(r"\{([^}]+)\}", r"[A-Za-z0-9_.-]*", path)
+        condition = f"request.url_path.matches('{pattern}')"
+    else:
+        condition = f"request.url_path == '{path}'"
+
+    return {
+        "value": path,
+        "condition": condition,
+    }
+
+
+def __request_apigroup_item(path):
+    if match := re.match(r"^\/(v[0-9]+)\/([^\/]+)(?:\/([^\/]+))*$", path):
+        pattern = "/v[0-9]+/" + (
+            f"{match.groups()[1]}/.*" if match.groups()[2] else f"{match.groups()[1]}"
+        )
+        return {
+            "value": match.groups()[1],
+            "condition": f"request.url_path.matches('{pattern}')",
+        }
+    else:
+        apigroup = path.split("/")[1]
+        return {
+            "value": apigroup,
+            "condition": f"request.url_path.matches('/{apigroup}/.*')",
+        }
